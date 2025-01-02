a44499d2aa253658a11f4b965cfe26bc4e6869d9
Aman09102001 <amanmandliya2001@gmail.com>
2025-01-02 11:21:42 -0800

    Lets Start

---
+++ script.js
@@ -0,0 +1,45 @@
+// var arr=[1,2,3,4,5,6,7,8];
+// var arr1=arr.filter((val)=>{
+//     if(val%2==0)
+//      return true;
+// })
+// console.log(arr1);
+
+
+// var arr3 = [1,2,3,4,5,6,7,8];
+// var arr4 = arr3.forEach((val)=>{        //foreach kuch bhi return nh krta h and return undefined
+//     console.log(val+10);
+    
+// })
+// console.log(arr4);
+
+// var arr3 = [1,2,3,4,5,6,7,8];
+// arr3.forEach((val)=>{        
+//     console.log(val+10);
+     
+// })
+
+
+//  var arr5= [1,2,3,4,5,6,7,8];                        // Map returns a new array
+//  var arr6=arr4.map((val)=>{
+//    return val+10;
+//  })
+//  console.log(arr6);
+
+// var arr7=[1,2,3,4,5,6,7];
+//  let res=arr7.find((val)=>{
+//     if(val==5)
+//         return val;
+//  });
+//  console.log(res);
+
+// var arr8=[1,2,3,4,5,6,7];
+//  let res=arr8.indexOf(5);
+//  console.log(res);
+ 
+ function abc(a,b,c){                  // It checks length of the function(parameter)
+    return 0;
+ }
+ console.log(abc.length);
+ 
+
